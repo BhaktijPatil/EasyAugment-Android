1919a284e2b6cee456a433fa5479b7fae9b6bd7b
Vaibhav Bagri <vaibhavbagri07@gmail.com>
2020-02-26 16:42:39 +0530

    Added video augmentation over markers

--- easy_augment/src/main/java/com/liminal/easy_augment/RedirectVideo.java
+++ easy_augment/src/main/java/com/liminal/easy_augment/RedirectVideo.java
@@ -3,53 +3,28 @@ package com.liminal.easy_augment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.ImageView;
 import android.widget.ProgressBar;
-import com.google.android.exoplayer2.DefaultLoadControl;
-import com.google.android.exoplayer2.DefaultRenderersFactory;
-import com.google.android.exoplayer2.ExoPlaybackException;
 import com.google.android.exoplayer2.ExoPlayerFactory;
-import com.google.android.exoplayer2.LoadControl;
-import com.google.android.exoplayer2.PlaybackParameters;
 import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.Timeline;
-import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.ProgressiveMediaSource;
-import com.google.android.exoplayer2.source.TrackGroupArray;
-import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
-import com.google.android.exoplayer2.trackselection.TrackSelection;
-import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
-import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.PlayerView;
-import com.google.android.exoplayer2.upstream.BandwidthMeter;
 import com.google.android.exoplayer2.upstream.DataSource;
-import com.google.android.exoplayer2.upstream.DefaultAllocator;
-import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
 
-import androidx.appcompat.app.AppCompatDelegate;
-
 public class RedirectVideo extends AppCompatActivity implements Player.EventListener{
-
 
     private PlayerView playerView;
     private SimpleExoPlayer player;
+    private ProgressBar spinnerVideoDetails;
     String vid_url;
-    private boolean playWhenReady = true;
-    private int currentWindow = 0;
-    private long playbackPosition = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,6 +35,7 @@ public class RedirectVideo extends AppCompatActivity implements Player.EventList
                         WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_redirect_video);
         playerView = findViewById(R.id.video_view);
+        spinnerVideoDetails = findViewById(R.id.spinnerVideoDetails);
         vid_url = getIntent().getStringExtra("VIDEO_URL");
     }
 
@@ -68,9 +44,10 @@ public class RedirectVideo extends AppCompatActivity implements Player.EventList
         playerView.setPlayer(player);
         Uri uri = Uri.parse(vid_url);
         MediaSource mediaSource = buildMediaSource(uri);
-        player.setPlayWhenReady(playWhenReady);
-        player.seekTo(currentWindow, playbackPosition);
+        player.setPlayWhenReady(true);
+        player.seekTo(0,0);
         player.prepare(mediaSource, false, false);
+        player.addListener(this);
     }
 
     private MediaSource buildMediaSource(Uri uri) {
@@ -108,9 +85,7 @@ public class RedirectVideo extends AppCompatActivity implements Player.EventList
     @Override
     public void onPause() {
         super.onPause();
-        if (Util.SDK_INT < 24) {
             releasePlayer();
-        }
     }
 
     @Override
@@ -123,143 +98,29 @@ public class RedirectVideo extends AppCompatActivity implements Player.EventList
 
     private void releasePlayer() {
         if (player != null) {
-            playWhenReady = player.getPlayWhenReady();
-            playbackPosition = player.getCurrentPosition();
-            currentWindow = player.getCurrentWindowIndex();
             player.release();
             player = null;
         }
     }
 
-
-
-//    private void setUp() {
-//        initializePlayer();
-//        if (videoUri == null) {
-//            return;
-//        }
-//        buildMediaSource(Uri.parse(videoUri));
-//    }
-//    @OnClick(R.id.imageViewExit)
-//    public void onViewClicked() {
-//        finish();
-//    }
-//    private void initializePlayer() {
-//        if (player == null) {
-//            // 1. Create a default TrackSelector
-//            LoadControl loadControl = new DefaultLoadControl(
-//                    new DefaultAllocator(true, 16),
-//                    VideoPlayerConfig.MIN_BUFFER_DURATION,
-//                    VideoPlayerConfig.MAX_BUFFER_DURATION,
-//                    VideoPlayerConfig.MIN_PLAYBACK_START_BUFFER,
-//                    VideoPlayerConfig.MIN_PLAYBACK_RESUME_BUFFER, -1, true);
-//            BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
-//            TrackSelection.Factory videoTrackSelectionFactory =
-//                    new AdaptiveTrackSelection.Factory(bandwidthMeter);
-//            TrackSelector trackSelector =
-//                    new DefaultTrackSelector(videoTrackSelectionFactory);
-//            // 2. Create the player
-//            player =
-//                    ExoPlayerFactory.newSimpleInstance(new DefaultRenderersFactory(this), trackSelector,
-//                            loadControl);
-//            videoFullScreenPlayer.setPlayer(player);
-//        }
-//    }
-//    private void buildMediaSource(Uri mUri) {
-//        // Measures bandwidth during playback. Can be null if not required.
-//        DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
-//        // Produces DataSource instances through which media data is loaded.
-//        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this,
-//                Util.getUserAgent(this, getString(R.string.app_name)), bandwidthMeter);
-//        // This is the MediaSource representing the media to be played.
-//        MediaSource videoSource = new ExtractorMediaSource.Factory(dataSourceFactory)
-//                .createMediaSource(mUri);
-//        // Prepare the player with the source.
-//        player.prepare(videoSource);
-//        player.setPlayWhenReady(true);
-//        player.addListener(this);
-//    }
-//    private void releasePlayer() {
-//        if (player != null) {
-//            player.release();
-//            player = null;
-//        }
-//    }
-//    private void pausePlayer() {
-//        if (player != null) {
-//            player.setPlayWhenReady(false);
-//            player.getPlaybackState();
-//        }
-//    }
-//    private void resumePlayer() {
-//        if (player != null) {
-//            player.setPlayWhenReady(true);
-//            player.getPlaybackState();
-//        }
-//    }
-//    @Override
-//    protected void onPause() {
-//        super.onPause();
-//        pausePlayer();
-//        if (mRunnable != null) {
-//            mHandler.removeCallbacks(mRunnable);
-//        }
-//    }
-//    @Override
-//    protected void onRestart() {
-//        super.onRestart();
-//        resumePlayer();
-//    }
-//    @Override
-//    protected void onDestroy() {
-//        super.onDestroy();
-//        releasePlayer();
-//    }
-//    @Override
-//    public void onTimelineChanged(Timeline timeline, Object manifest, int reason) {
-//    }
-//    @Override
-//    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
-//    }
-//    @Override
-//    public void onLoadingChanged(boolean isLoading) {
-//    }
-//    @Override
-//    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
-//        switch (playbackState) {
-//            case Player.STATE_BUFFERING:
-//                spinnerVideoDetails.setVisibility(View.VISIBLE);
-//                break;
-//            case Player.STATE_ENDED:
-//                // Activate the force enable
-//                break;
-//            case Player.STATE_IDLE:
-//                break;
-//            case Player.STATE_READY:
-//                spinnerVideoDetails.setVisibility(View.GONE);
-//                break;
-//            default:
-//                // status = PlaybackStatus.IDLE;
-//                break;
-//        }
-//    }
-//    @Override
-//    public void onRepeatModeChanged(int repeatMode) {
-//    }
-//    @Override
-//    public void onShuffleModeEnabledChanged(boolean shuffleModeEnabled) {
-//    }
-//    @Override
-//    public void onPlayerError(ExoPlaybackException error) {
-//    }
-//    @Override
-//    public void onPositionDiscontinuity(int reason) {
-//    }
-//    @Override
-//    public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
-//    }
-//    @Override
-//    public void onSeekProcessed() {
-//    }
-
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+        switch (playbackState) {
+            case Player.STATE_BUFFERING:
+                spinnerVideoDetails.setVisibility(View.VISIBLE);
+                break;
+            case Player.STATE_ENDED:
+                spinnerVideoDetails.setVisibility(View.GONE);
+                break;
+            case Player.STATE_IDLE:
+                spinnerVideoDetails.setVisibility(View.GONE);
+                break;
+            case Player.STATE_READY:
+                spinnerVideoDetails.setVisibility(View.GONE);
+                break;
+            default:
+                spinnerVideoDetails.setVisibility(View.GONE);
+                break;
+        }
+    }
 }
--- easy_augment/src/main/java/com/liminal/easy_augment/ScanActivity.java
+++ easy_augment/src/main/java/com/liminal/easy_augment/ScanActivity.java
@@ -4,6 +4,7 @@ package com.liminal.easy_augment;
 
 import android.content.ComponentName;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,9 +12,22 @@ import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.ar.core.Anchor;
 import com.google.ar.core.AugmentedImage;
 import com.google.ar.core.Frame;
+import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.FrameTime;
+import com.google.ar.sceneform.Scene;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.ExternalTexture;
+import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 
 import java.util.Collection;
@@ -24,6 +38,15 @@ public class ScanActivity extends AppCompatActivity {
 
     private ArFragment arFragment;
     private ImageView scannerView;
+
+    private ExternalTexture texture;
+    private ModelRenderable renderable;
+    private SimpleExoPlayer player;
+    private Scene scene;
+
+    private boolean isTracking = false;
+    private boolean augmentVideo = false;
+
     private final Map<AugmentedImage, AugmentedImageNode> augmentedImageMap = new HashMap<>();
 
     @Override
@@ -35,7 +58,8 @@ public class ScanActivity extends AppCompatActivity {
         scannerView = findViewById(R.id.image_view_fit_to_scan);
 
         if (arFragment != null) {
-            arFragment.getArSceneView().getScene().addOnUpdateListener(this::onUpdateFrame);
+            scene = arFragment.getArSceneView().getScene();
+            scene.addOnUpdateListener(this::onUpdateFrame);
         }
     }
 
@@ -95,6 +119,11 @@ public class ScanActivity extends AppCompatActivity {
                         case "3": // Augment Model
                             Log.d("SCAN_ACTIVITY_REDIRECT_TO", "Augmenting model : " + DBManager.getDownloadedFromImageDetails("redirectURL").get(augmentedImage.getIndex()));
                             break;
+
+                        case "4": // Augment Video
+                            Log.d("SCAN_ACTIVITY_REDIRECT_TO", "Augmenting video : " + DBManager.getDownloadedFromImageDetails("redirectURL").get(augmentedImage.getIndex()));
+                            videoAugment(DBManager.getDownloadedFromImageDetails("redirectURL").get(augmentedImage.getIndex()));
+                            break;
                     }
                     break;
 
@@ -104,10 +133,20 @@ public class ScanActivity extends AppCompatActivity {
                     // Create a new anchor for newly found images.
                     if (!augmentedImageMap.containsKey(augmentedImage))
                     {
-                        AugmentedImageNode node = new AugmentedImageNode(this, DBManager.getDownloadedFromImageDetails("redirectURL").get(augmentedImage.getIndex()));
-                        node.setImage(augmentedImage);
-                        augmentedImageMap.put(augmentedImage, node);
-                        arFragment.getArSceneView().getScene().addChild(node);
+                        if(!augmentVideo)
+                        {
+                            AugmentedImageNode node = new AugmentedImageNode(this, DBManager.getDownloadedFromImageDetails("redirectURL").get(augmentedImage.getIndex()));
+                            node.setImage(augmentedImage);
+                            augmentedImageMap.put(augmentedImage, node);
+                            arFragment.getArSceneView().getScene().addChild(node);
+                        }
+                        else
+                            if(!isTracking)
+                            {
+                                playVideo(augmentedImage.createAnchor(augmentedImage.getCenterPose()), augmentedImage.getExtentX(),
+                                        augmentedImage.getExtentZ());
+                                isTracking = true;
+                            }
                     }
                     break;
 
@@ -125,5 +164,55 @@ public class ScanActivity extends AppCompatActivity {
         super.onStop();
         // Destroy the scanner activity once an image is found
         finish();
+    }
+
+    protected void videoAugment(String url){
+        if(player == null)
+        {
+            texture = new ExternalTexture();
+            Uri uri = Uri.parse(url);
+            MediaSource mediaSource = buildMediaSource(uri);
+            player = ExoPlayerFactory.newSimpleInstance(this);
+            player.setVideoSurface(texture.getSurface());
+            player.prepare(mediaSource, false, false);
+            player.setRepeatMode(Player.REPEAT_MODE_ALL);
+            player.setPlayWhenReady(false);
+            augmentVideo = true;
+
+            ModelRenderable
+                    .builder()
+                    .setSource(this, R.raw.augmented_video_model)
+                    .build()
+                    .thenAccept(modelRenderable -> {
+                        modelRenderable.getMaterial().setExternalTexture("videoTexture",
+                                texture);
+
+                        renderable = modelRenderable;
+                    });
+        }
+    }
+
+    private MediaSource buildMediaSource(Uri uri) {
+        DataSource.Factory dataSourceFactory =
+                new DefaultDataSourceFactory(this, "exoplayer-codelab");
+        return new ProgressiveMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(uri);
+    }
+
+    private void playVideo(Anchor anchor, float extentX, float extentZ) {
+
+        player.setPlayWhenReady(true);
+
+        AnchorNode anchorNode = new AnchorNode(anchor);
+
+        texture.getSurfaceTexture().setOnFrameAvailableListener(surfaceTexture -> {
+            anchorNode.setRenderable(renderable);
+            texture.getSurfaceTexture().setOnFrameAvailableListener(null);
+        });
+
+        anchorNode.setWorldScale(new Vector3(extentX, 1f, extentZ));
+
+        scene.addChild(anchorNode);
+
     }
 }
