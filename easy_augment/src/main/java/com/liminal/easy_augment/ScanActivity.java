5d95728a6bd2b8fdd17e2427a8ef40ec0b812b01
BhaktijPatil <bhaktij1999@gmail.com>
2020-02-28 14:53:57 +0530

    Loading Bar added

--- easy_augment/src/main/java/com/liminal/easy_augment/ScanActivity.java
+++ easy_augment/src/main/java/com/liminal/easy_augment/ScanActivity.java
@@ -8,11 +8,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.ar.core.AugmentedImage;
 import com.google.ar.core.Frame;
+import com.bumptech.glide.Glide;
 import com.google.ar.sceneform.FrameTime;
 import com.google.ar.sceneform.Scene;
 import com.google.ar.sceneform.ux.ArFragment;
@@ -20,6 +23,8 @@ import com.google.ar.sceneform.ux.ArFragment;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+
+import static com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade;
 
 public class ScanActivity extends AppCompatActivity {
 
@@ -30,6 +35,8 @@ public class ScanActivity extends AppCompatActivity {
 
     private boolean isTracking = false;
     private boolean augmentVideo = false;
+    RelativeLayout loadingBar;
+    TextView loadingText;
 
     private final Map<AugmentedImage, AugmentedImageNode> augmentedImageMap = new HashMap<>();
 
@@ -37,6 +44,9 @@ public class ScanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
+
+        // Initializes the loading bar
+        setupLoadingBar();
 
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
         scannerView = findViewById(R.id.scanner_view);
@@ -47,30 +57,23 @@ public class ScanActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (augmentedImageMap.isEmpty())
-        {
-            scannerView.setVisibility(View.VISIBLE);
-        }
-    }
-
     //This method is called at the start of each frame. @param frameTime = time since last frame.
     private void onUpdateFrame(FrameTime frameTime) {
         Frame frame = arFragment.getArSceneView().getArFrame();
-        // If there is no frame, just return.
-        if (frame == null) {
+        // Return if no frame is obtained
+        if (frame == null)
             return;
-        }
 
         Collection<AugmentedImage> updatedAugmentedImages = frame.getUpdatedTrackables(AugmentedImage.class);
 
         for (AugmentedImage augmentedImage : updatedAugmentedImages) {
             switch (augmentedImage.getTrackingState()) {
-
                 case PAUSED: //image has been detected.
                     Log.d("SCAN_ACTIVITY_TRACKING_STATE", "tracking state : PAUSED");
+
+                    if(loadingBar.getVisibility() == View.GONE)
+                        setLoadingBarVisible("Image Found. Processing Environment");
+
                     switch (DBManager.getDownloadedFromImageDetails("redirectTo").get(augmentedImage.getIndex())) {
                         case "0": // Open Activity
                             Intent intent = getIntent();
@@ -114,9 +117,26 @@ public class ScanActivity extends AppCompatActivity {
                     break;
 
                 case TRACKING: // Image is being tracked
-                    Log.d("SCAN_ACTIVITY_TRACKING_STATE", "tracking state : TRACKING");
-                    scannerView.setVisibility(View.GONE);
-                    // Create a new anchor for newly found images.
+                    Log.d("SCAN_ACTIVITY_TRACKING_STATE", "tracking state : " + augmentedImage.getTrackingMethod());
+
+                    // Set visibility of scanner depending on tracking status
+                    if(augmentedImage.getTrackingMethod() == AugmentedImage.TrackingMethod.FULL_TRACKING)
+                    {
+                        if(loadingBar.getVisibility() == View.VISIBLE)
+                        {
+                            loadingBar.setVisibility(View.GONE);
+                            scannerView.setVisibility(View.GONE);
+                        }
+                    }
+                    else if(augmentedImage.getTrackingMethod() == AugmentedImage.TrackingMethod.LAST_KNOWN_POSE)
+                    {
+                        if(loadingBar.getVisibility() == View.GONE)
+                        {
+                            scannerView.setVisibility(View.VISIBLE);
+                            setLoadingBarVisible("Lost Marker. Searching...");
+                        }
+                    }
+
                     if (!augmentedImageMap.containsKey(augmentedImage))
                     {
                         if(!augmentVideo)
@@ -126,13 +146,11 @@ public class ScanActivity extends AppCompatActivity {
                             augmentedImageMap.put(augmentedImage, node);
                             arFragment.getArSceneView().getScene().addChild(node);
                         }
-                        else
-                            if(!isTracking)
-                            {
-                                AugmentVideo.playVideo(augmentedImage.createAnchor(augmentedImage.getCenterPose()), augmentedImage.getExtentX(),
-                                        augmentedImage.getExtentZ(), scene);
-                                isTracking = true;
-                            }
+                        else if(!isTracking)
+                        {
+                            AugmentVideo.playVideo(augmentedImage.createAnchor(augmentedImage.getCenterPose()), augmentedImage.getExtentX(), augmentedImage.getExtentZ(), scene);
+                            isTracking = true;
+                        }
                     }
                     break;
 
@@ -144,12 +162,40 @@ public class ScanActivity extends AppCompatActivity {
         }
     }
 
-    // On stop method is called when the activity is paused (Image is detected)
+    // Function to setup loading bar
+    private void setupLoadingBar()
+    {
+        ImageView loadingImgView = findViewById(R.id.loadingImgView);
+        loadingText = findViewById(R.id.loadingTextView);
+        loadingBar = findViewById(R.id.loadingBar);
+
+        Glide.with(this)
+                .load(R.drawable.loading_spinner)
+                .centerCrop()
+                .transition(withCrossFade())
+                .into(loadingImgView);
+    }
+
+    // Function to set Text in the loading bar and make it visible
+    private void setLoadingBarVisible(String text)
+    {
+        loadingBar.setVisibility(View.VISIBLE);
+        loadingText.setText(text);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (augmentedImageMap.isEmpty())
+        {
+            scannerView.setVisibility(View.VISIBLE);
+        }
+    }
+
     @Override
     protected void onStop() {
         super.onStop();
         // Destroy the scanner activity once an image is found
         finish();
     }
-
 }
