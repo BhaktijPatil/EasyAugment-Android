690d3f020c02a19fa6140ff1ee978ea95035d7b7
Vaibhav Bagri <vaibhavbagri07@gmail.com>
2020-03-04 12:58:22 +0530

    Fragment augmentation added

---
+++ app/src/main/java/com/liminal/easy_augment_example/AugmentedFragment.java
@@ -0,0 +1,24 @@
+package com.liminal.easy_augment_example;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class AugmentedFragment extends Fragment {
+    private TextView textView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment, container, false);
+        textView = view.findViewById(R.id.textView);
+        textView.setText("first");
+        return view;
+    }
+}
